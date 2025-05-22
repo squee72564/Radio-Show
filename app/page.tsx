563ef9a21cd4c0c4a0ca00570a39fa7e3e03d96e
Alex Rodriguez <rodriguez.al183@gmail.com>
2025-05-22 16:16:15 -0700

    Add initial home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,28 @@
+import Link from "next/link";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      test
+    <div className="flex flex-col items-center justify-items-center min-h-screen p-8 gap-8 bg-blue-200 text-black">
+      <h1 className="font-bold text-[48px] text-center">Welcome to Radio Show</h1>
+      <div className="flex flex-col md:flex-row justify-center items-center gap-5 font-bold">
+        <Image
+          src={"/radio-show.jpg"}
+          alt="Image"
+          width={640}
+          height={640}
+          className="max-w-[30%] min-w-[25%] rounded-[100px]"
+        />
+        <div className="flex flex-col gap-10 text-center">
+          <p>Radio Show is a free internet radio platform for everyone.</p>
+          <p>Listen to live music, browse archives, or host your own show.</p>
+        </div>
+      </div>
+      <Button className="text-white bg-black hover:bg-gray-800">
+        <Link href={"/dashboard"}>Click here to enter</Link>
+      </Button>
+      
     </div>
   );
 }

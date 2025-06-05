ee9d266e921180c223baab27bc2c358d632fc1d6
Alex Rodriguez <rodriguez.al183@gmail.com>
2025-06-05 09:56:10 -0700

    Try and fix prisma client

--- webapp/lib/db/prismaClient.ts
+++ webapp/lib/db/prismaClient.ts
@@ -1,7 +1,14 @@
-import { PrismaClient } from "@prisma/client";
- 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
- 
-export const prisma = globalForPrisma.prisma || new PrismaClient()
- 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
+import { PrismaClient } from "@prisma/client"
+
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient
+}
+
+export const prisma =
+  globalForPrisma.prisma ?? new PrismaClient({
+  log: ['query', 'error', 'warn'],
+})
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma
+}

dec4b74c4e84809477146995610ad72293155a82
Alex Rodriguez <rodriguez.al183@gmail.com>
2025-06-08 19:05:27 -0700

    Delete unused db functions

--- webapp/lib/db/services/icecastActions.ts
+++
@@ -1,9 +0,0 @@
-import { prisma } from "@/lib/db/prismaClient";
-
-export async function findConfigById(id: string) {
-  return prisma.systemConfig.findUnique({ where: { id } });
-}
-
-export async function findServerById (id: string) {
-  return prisma.icecastServer.findUnique({ where: { id } });
-}

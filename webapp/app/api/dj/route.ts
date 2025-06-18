422a6fa61e9b64560b92afc3fc55ff49ba88bca8
Alex Rodriguez <rodriguez.al183@gmail.com>
2025-06-17 19:44:53 -0700

    Update dj route for authentication when a user connects

--- webapp/app/api/dj/route.ts
+++ webapp/app/api/dj/route.ts
@@ -1,27 +1,78 @@
+import { prisma } from "@/lib/db/prismaClient";
+
 export async function POST(req: Request) {
   const bodyText = await req.text();
+  const body: {password: string, user: string, address: string}
+    = JSON.parse(bodyText);
 
-  const params = new URLSearchParams(bodyText);
-  const user = params.get("user");
-  const pass = params.get("pass");
+  const userId = body.user;
+  const password = body.password;
 
-  console.log("Auth request:", {user, pass});
+  if (!userId || !password) {
+    return new Response(JSON.stringify({authenticated: false, message: "No user or pass"}), {
+      status: 401,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
 
-  // Replace with actual DB logic based on user credentials and
-  // show table in db
+  const streamschedule = await prisma.streamSchedule.findUnique({
+    where: {
+      userId_password: {
+        userId: userId,
+        password: password,
+      },
+    }
+  })
+
+  if (!streamschedule) {
+    return new Response(JSON.stringify({authenticated: false, message: "Could not find stream schedule"}), {
+      status: 401,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+  
+  const now = new Date();
+
+  const streamInstances = await prisma.streamInstance.findMany({
+    where: {
+      streamScheduleId: streamschedule.id,
+      scheduledStart: { lte: now },
+      scheduledEnd: { gte: now },
+    },
+  });
+
+  console.log(streamInstances);
+
+  if (!streamInstances || streamInstances.length === 0) {
+    return new Response(JSON.stringify({authenticated: false, message: "Could not find stream instance"}), {
+      status: 401,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
+  const streamInstance = streamInstances[0];
+
+  const streamScheduleId = streamschedule.id;
+  const streamInstanceId = streamInstance.id;
+  const title = streamschedule.title;
+  const timelimit = Math.max(
+    0,
+    Math.floor((streamInstance.scheduledEnd.getTime() - Date.now()) / 1000)
+  );
+
   const data = {
     authenticated: true,
     meta: {
-      title: "Test Title From Api",
-      user: "Test User From API",
-      timelimit: 1.0 * 60.0 * 60.0
+      title,
+      timelimit,
+      userId,
+      streamScheduleId,
+      streamInstanceId,
     }
   };
 
   return new Response(JSON.stringify(data), {
-    status: data.authenticated ? 200 : 401,
-    headers: {
-      "Content-Type": "application/json",
-    },
+    status: 200,
+    headers: { "Content-Type": "application/json" },
   });
 }

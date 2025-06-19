3506440cf33fcb8c4906d3e8fb8573293028d222
Alex Rodriguez <rodriguez.al183@gmail.com>
2025-06-19 13:35:27 -0700

    Remove unused import

--- webapp/app/(radio-show)/live/page.tsx
+++ webapp/app/(radio-show)/live/page.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Music } from "lucide-react";
 
-import { Card, CardContent } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 import LiveStreamPlayer from "@/app/(radio-show)/live/live-stream-player";
 

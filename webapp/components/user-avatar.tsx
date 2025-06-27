b5ecad549deec774dbd600359c8cc5ad4edfcbd4
Alex Rodriguez <rodriguez.al183@gmail.com>
2025-06-27 12:28:11 -0700

    Hide aria for user avatar as alt is repeated

--- webapp/components/user-avatar.tsx
+++ webapp/components/user-avatar.tsx
@@ -4,7 +4,7 @@ import { User } from "@prisma/client";
 export default function UserAvatar({user, className}: {user: User, className?: string}) {
   return (
     <Avatar className={className}>
-      <AvatarImage src={user.image || undefined} alt={user.name || "user-avatar"} />
+      <AvatarImage src={user.image || undefined} alt={user.name || "user-avatar"} aria-hidden={true}/>
       <AvatarFallback>{user.name?.charAt(0) ?? "?"}</AvatarFallback>
     </Avatar>
   );

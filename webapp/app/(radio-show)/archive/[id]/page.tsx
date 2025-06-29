4bf97c8bc23ab62132bc5430dad05fe0c6e5733e
Alex Rodriguez <rodriguez.al183@gmail.com>
2025-06-29 12:51:59 -0700

    Edit stream archive page; works now with S3

--- webapp/app/(radio-show)/archive/[id]/page.tsx
+++ webapp/app/(radio-show)/archive/[id]/page.tsx
@@ -21,32 +21,35 @@ export default async function ArchivePlayer({
       streamSchedule: true,
       streamInstance: true,
     }
-  }) as (StreamArchive & Partial<StreamArchiveRelations>) | null;
+  }) as (StreamArchive & StreamArchiveRelations) | null;
 
+  if (!streamArchive) {
+    return (
+    <Badge variant={"outline"} className="text-lg font-bold">
+      No stream archive found.
+    </Badge>
+    )
+  }
+  
   return (
     <main className="flex flex-col min-h-screen w-full p-6 gap-6">
       <h1 className="text-2xl font-bold flex items-center gap-2">
         <ArchiveIcon className="w-6 h-6" />
-        Archive {streamArchive?.streamInstance && <LocalDate date={streamArchive.streamInstance?.scheduledStart}/>}
+        Archive {streamArchive.streamInstance && <LocalDate date={streamArchive.streamInstance.scheduledStart}/>}
       </h1>
       <Separator />
-      {!streamArchive ? (
-        <Badge variant={"outline"} className="text-lg font-bold">
-          No stream archive found.
-        </Badge>
-      ): (
-        <div className="flex flex-col flex-1 gap-4 w-full">
-          <CustomPlayer streamUrl={`/${streamArchive.url.replace(/^public\//, "")}`} isStreamLive={true} showControls={true}/>
-          <Alert variant="default" className="w-full">
-            <AlertTitle>{streamArchive.streamSchedule?.title} - {streamArchive.user?.name}</AlertTitle>
-            <AlertDescription className="text-base">
-              <p className="">
-                {streamArchive.streamSchedule?.description}
-              </p>
-            </AlertDescription>
-          </Alert>
-        </div>
-      )}
+
+      <div className="flex flex-col flex-1 gap-4 w-full">
+        <CustomPlayer streamUrl={`/api/archive?archiveId=${streamArchive.id}`} isStreamLive={true} showControls={true}/>
+        <Alert variant="default" className="w-full">
+          <AlertTitle>{streamArchive.streamSchedule.title} - {streamArchive.user.name}</AlertTitle>
+          <AlertDescription className="text-base">
+            <p className="">
+              {streamArchive.streamSchedule.description}
+            </p>
+          </AlertDescription>
+        </Alert>
+      </div>
     </main>
   );
 }

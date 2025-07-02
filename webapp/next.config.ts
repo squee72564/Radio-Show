dcf397bdbf6a0f0fdd14eaf4c977701ca4d5aa1b
Alex Rodriguez <rodriguez.al183@gmail.com>
2025-07-01 17:57:39 -0700

    Use dotenv and NEXT_ENV mode to select correct .env file

--- webapp/next.config.ts
+++ webapp/next.config.ts
@@ -1,4 +1,14 @@
 import type { NextConfig } from "next";
+import path from "path";
+import dotenv from "dotenv";
+
+const nodeEnvMap = {
+  "production": "prod",
+  "development": "dev",
+  "test": "test"
+};
+
+dotenv.config({path: `../.env.${nodeEnvMap[process.env.NODE_ENV!]}`})
 
 const nextConfig: NextConfig = {
   /* config options here */

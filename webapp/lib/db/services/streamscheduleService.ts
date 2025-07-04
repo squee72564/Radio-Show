f90ef0828461cb4c0c6f7e3f58f1c39221942e15
Alex Rodriguez <rodriguez.al183@gmail.com>
2025-07-03 18:23:28 -0700

    reorganize by table

--- webapp/lib/db/services/streamscheduleService.ts
+++ webapp/lib/db/services/streamscheduleService.ts
@@ -2,34 +2,27 @@ import { prisma } from "@/lib/db/prismaClient";
 import { StreamArchiveRelations, StreamInstanceRelations, StreamScheduleRelations } from "@/types/prisma-relations";
 import { $Enums, StreamArchive, StreamInstance, StreamSchedule } from "@prisma/client";
 
-export async function deleteArchiveById(id: string) {
+// Stream schedule
+export async function createStreamSchedule(data: Omit<StreamSchedule, "id">) {
   try {
-    return await prisma.streamArchive.delete({
-      where: {id}
+    return await prisma.streamSchedule.create({
+      data
     });
   } catch (err) {
-    console.error(err);
+    console.log(err);
     return null;
   }
-
 }
 
-export async function findAllStreamArchives(
-  options?: { include?: { [K in keyof StreamArchiveRelations]?: true } }
-): Promise<(StreamArchive & Partial<StreamArchiveRelations>)[]> {
-  return await prisma.streamArchive.findMany({
-    ...(options ?? {}),
-  });
-}
-
-export async function findStreamArchiveById(
-  id: string,
-  options?: { include?: { [K in keyof StreamArchiveRelations]?: true } }
-): Promise<(StreamArchive & Partial<StreamArchiveRelations>) | null> {
-  return await prisma.streamArchive.findUnique({
-    where: { id },
-    ...(options ?? {}),
-  });
+export async function deleteStreamScheduleById(id: string) {
+  try {
+    return await prisma.streamSchedule.delete({
+      where: {id}
+    })
+  } catch (err) {
+    console.log(err)
+    return null;
+  }
 }
 
 export async function findStreamScheduleByIdAndPass(
@@ -48,18 +41,51 @@ export async function findStreamScheduleByIdAndPass(
   });
 }
 
-export async function deleteStreamById(id: string) {
+export async function setStreamScheduleReviewedAt(id: string, reviewedAt: Date) {
   try {
-    return await prisma.streamSchedule.delete({
-      where: {id}
-    })
+    return prisma.streamSchedule.update({
+      where: {id},
+      data: {
+        reviewedAt
+      }
+    });
   } catch (err) {
     console.log(err)
     return null;
   }
-
 }
 
+export async function setStreamScheduleStatus(id: string, status: $Enums.ScheduleStatus) {
+  return await prisma.streamSchedule.update({
+    where: {id},
+    data: {status}
+  });
+}
+
+
+export async function findAllStreamsByStatus(
+  status: $Enums.ScheduleStatus,
+  options?: {include: {[K in keyof StreamScheduleRelations]?: true}}
+): Promise<(StreamSchedule & Partial<StreamScheduleRelations>)[]> {
+  return await prisma.streamSchedule.findMany({
+    where: {status: status},
+    ...(options ?? {}),
+  })
+}
+
+export async function findAllStreamsByStatusAndUser(userId: string, status: $Enums.ScheduleStatus) {
+  return await prisma.streamSchedule.findMany({
+    where: {status: status, userId: userId},
+  })
+}
+
+export async function getStreamCountByStatus(status: $Enums.ScheduleStatus) {
+  return await prisma.streamSchedule.count({
+    where: { status: status },
+  });
+}
+
+// Stream Instance
 export async function getStreamInstancesByDateRange(
   dateStart: Date, 
   dateEnd: Date,
@@ -79,20 +105,7 @@ export async function getStreamInstancesByDateRange(
   });
 }
 
-export async function setStreamScheduleReviewedAt(id: string, reviewedAt: Date) {
-  try {
-    return prisma.streamSchedule.update({
-      where: {id},
-      data: {
-        reviewedAt
-      }
-    });
-  } catch (err) {
-    console.log(err)
-    return null;
-  }
 
-}
 
 export async function getCurrentStreamInstance(
   options?: { include?: { [K in keyof StreamInstanceRelations]?: true } }
@@ -117,12 +130,6 @@ export async function getCurrentStreamInstance(
   });
 }
 
-export async function setStreamScheduleStatus(id: string, status: $Enums.ScheduleStatus) {
-  return await prisma.streamSchedule.update({
-    where: {id},
-    data: {status}
-  });
-}
 
 export async function revokeStreamInstances(streamId: string) {
   return await prisma.streamInstance.deleteMany({
@@ -130,27 +137,6 @@ export async function revokeStreamInstances(streamId: string) {
   })
 }
 
-export async function findAllStreamsByStatus(
-  status: $Enums.ScheduleStatus,
-  options?: {include: {[K in keyof StreamScheduleRelations]?: true}}
-): Promise<(StreamSchedule & Partial<StreamScheduleRelations>)[]> {
-  return await prisma.streamSchedule.findMany({
-    where: {status: status},
-    ...(options ?? {}),
-  })
-}
-
-export async function findAllStreamsByStatusAndUser(userId: string, status: $Enums.ScheduleStatus) {
-  return await prisma.streamSchedule.findMany({
-    where: {status: status, userId: userId},
-  })
-}
-
-export async function getStreamCountByStatus(status: $Enums.ScheduleStatus) {
-  return await prisma.streamSchedule.count({
-    where: { status: status },
-  });
-}
 
 export async function populateStreamInstances(
   validatedInstances: {
@@ -216,18 +202,6 @@ export async function isStreamInstancesConflicting(
   return !!conflicts;
 }
 
-export async function createStreamSchedule(data: Omit<StreamSchedule, "id">) {
-  try {
-    return await prisma.streamSchedule.create({
-      data
-    });
-  } catch (err) {
-    console.log(err);
-    return null;
-  }
-
-}
-
 export async function getStreamScheduleById(
   id: string,
   options?: {include: {[K in keyof StreamScheduleRelations]?: true}}
@@ -236,29 +210,6 @@ export async function getStreamScheduleById(
     where: {id: id},
     ...(options ?? {}),
   })
-}
-
-export async function findArchivesByUserId(
-  userId: string,
-  options?: {include: {[K in keyof StreamArchiveRelations]?: true}}
-): Promise<(StreamArchive & Partial<StreamArchiveRelations>)[]> {
-  return await prisma.streamArchive.findMany({
-    where: {userId},
-    ...(options ?? {}),
-  }); 
-}
-
-export async function findRecentArchives(
-  count: number,
-  options?: {include: {[K in keyof StreamArchiveRelations]?: true}}
-): Promise<(StreamArchive & Partial<StreamArchiveRelations>)[]> {
-  return await prisma.streamArchive.findMany({
-    orderBy: {
-      createdAt: 'desc',
-    },
-    ...(options ?? {}),
-    take: count,
-  });
 }
 
 export async function findFirstStreamInstanceAfterDate(date: Date) {
@@ -290,6 +241,7 @@ export async function findFirstStreamInstanceAfterDate(date: Date) {
   });
 }
 
+// Stream Archive
 export async function createStreamArchive(data: Omit<StreamArchive, "id">) {
   try {
     return await prisma.streamArchive.create({
@@ -299,4 +251,56 @@ export async function createStreamArchive(data: Omit<StreamArchive, "id">) {
     console.log(err)
     return null;
   }
+}
+
+export async function deleteArchiveById(id: string) {
+  try {
+    return await prisma.streamArchive.delete({
+      where: {id}
+    });
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+}
+
+export async function findAllStreamArchives(
+  options?: { include?: { [K in keyof StreamArchiveRelations]?: true } }
+): Promise<(StreamArchive & Partial<StreamArchiveRelations>)[]> {
+  return await prisma.streamArchive.findMany({
+    ...(options ?? {}),
+  });
+}
+
+export async function findStreamArchiveById(
+  id: string,
+  options?: { include?: { [K in keyof StreamArchiveRelations]?: true } }
+): Promise<(StreamArchive & Partial<StreamArchiveRelations>) | null> {
+  return await prisma.streamArchive.findUnique({
+    where: { id },
+    ...(options ?? {}),
+  });
+}
+
+export async function findArchivesByUserId(
+  userId: string,
+  options?: {include: {[K in keyof StreamArchiveRelations]?: true}}
+): Promise<(StreamArchive & Partial<StreamArchiveRelations>)[]> {
+  return await prisma.streamArchive.findMany({
+    where: {userId},
+    ...(options ?? {}),
+  }); 
+}
+
+export async function findRecentArchives(
+  count: number,
+  options?: {include: {[K in keyof StreamArchiveRelations]?: true}}
+): Promise<(StreamArchive & Partial<StreamArchiveRelations>)[]> {
+  return await prisma.streamArchive.findMany({
+    orderBy: {
+      createdAt: 'desc',
+    },
+    ...(options ?? {}),
+    take: count,
+  });
 }

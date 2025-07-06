463aa58d8d1b99fcb67889db9f5942e6a75782aa
Alex Rodriguez <rodriguez.al183@gmail.com>
2025-07-06 03:09:23 -0700

    Change css for archive list

--- webapp/app/(radio-show)/dashboard/recent-archives.tsx
+++ webapp/app/(radio-show)/dashboard/recent-archives.tsx
@@ -22,7 +22,7 @@ export default async function RecentArchives() {
     <section className="min-h-41 max-h-41 w-full">
       <h2 className="text-lg font-medium mb-4">Recent Archives</h2>
       {archives.length > 0 ? (
-        <div className="list-disc space-y-1 overflow-auto">
+        <div className="flex flex-col gap-2 overflow-auto">
           {archives.map((archive, idx) => (
             <StreamArchiveInfoCard key={idx} streamArchive={archive}/>
           ))}
